e08918ed33055d5fc1bd21d31b48a2ed2b271c2b
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-09-20 12:57:24 -0400

    Add functions for conversions

---
+++ src/modules/conversions.js
@@ -0,0 +1,24 @@
+function roundTemperature(temp) {
+  return Math.round(temp) + String.fromCharCode(176);
+}
+
+function roundHumidity(humidity) {
+  return Math.round(humidity) + "%";
+}
+
+function formatTime(time) {
+  let [hours, minutes] = time.split(":").map(Number);
+  const period = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  return `${hours}:${minutes.toString().padStart(2, "0")} ${period}`;
+}
+
+function convertTemperatureToCelsius(temp) {
+  const tempInC = ((temp - 32) * 5) / 9;
+  return roundTemperature(tempInC);
+}
+
+function convertTemperatureToFaharenheit(temp) {
+  const tempInF = temp * (9 / 5) + 32;
+  return roundTemperature(tempInF);
+}
