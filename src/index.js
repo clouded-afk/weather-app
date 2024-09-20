b8794fe40aeb42b0ef959250ed50d791c153995c
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-09-20 17:10:06 -0400

    Add eventlistener to convert temp button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import "./styles.css";
-import { changeLocation } from "./modules/domManipulation";
+import {
+  changeLocation,
+  displayTemperatureConversion,
+} from "./modules/domManipulation";
 
 const searchButton = document.querySelector(".search-button");
 
 searchButton.addEventListener("click", changeLocation);
+
+const temperatureButton = document.querySelector(".convert-temp-button");
+
+temperatureButton.addEventListener("click", () => {
+  const temperature = document.querySelector(".temperature-data").textContent;
+  const feelsLike = document.querySelector(".feels-like-data").textContent;
+  displayTemperatureConversion(temperature, feelsLike);
+});
