b316f27d1434fab518a2942e4d204e196f8e8e1e
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-09-20 12:55:57 -0400

    Remove conversion functions

--- src/modules/domManipulation.js
+++ src/modules/domManipulation.js
@@ -46,21 +46,6 @@ function selectWeatherIcon(icon) {
   return icons[icon];
 }
 
-function roundTemperature(temp) {
-  return Math.round(temp) + String.fromCharCode(176);
-}
-
-function roundHumidity(humidity) {
-  return Math.round(humidity) + "%";
-}
-
-function formatTime(time) {
-  let [hours, minutes] = time.split(":").map(Number);
-  const period = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12;
-  return `${hours}:${minutes.toString().padStart(2, "0")} ${period}`;
-}
-
 function displayWeatherData(
   location,
   conditions,
