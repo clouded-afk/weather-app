eee2a5b437a8829b2fe4356ae96712fb50ccf3c8
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-09-23 15:03:54 -0400

    Add try/catch to handle errors

--- src/modules/apiFunctions.js
+++ src/modules/apiFunctions.js
@@ -6,43 +6,58 @@ const visualCrossingURL =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
 
 async function getWeather(location) {
-  const response = await fetch(
-    `${visualCrossingURL}/${location}?key=${apiKey}`,
-    {
-      mode: "cors",
-    },
-  );
-  const weatherData = await response.json();
+  const errorDisplay = document.querySelector(".error-message");
+  const weatherDisplay = document.querySelector(".weather-display-container");
+  const tempConversionButton = document.querySelector(".convert-temp-button");
 
-  console.log(weatherData);
+  try {
+    const response = await fetch(
+      `${visualCrossingURL}/${location}?key=${apiKey}`,
+      {
+        mode: "cors",
+      },
+    );
 
-  const locationData = new Weather(
-    weatherData.resolvedAddress,
-    weatherData["currentConditions"].temp,
-    weatherData["currentConditions"].feelslike,
-    weatherData["currentConditions"].conditions,
-    weatherData["currentConditions"].humidity,
-    weatherData["currentConditions"].sunrise,
-    weatherData["currentConditions"].sunset,
-    weatherData.description,
-    weatherData["currentConditions"].icon,
-    weatherData["days"][0].precipprob,
-  );
+    const weatherData = await response.json();
 
-  console.log(locationData);
+    console.log(weatherData);
 
-  displayWeatherData(
-    locationData.address,
-    locationData.conditions,
-    locationData.temp,
-    locationData.feelsLike,
-    locationData.description,
-    locationData.sunrise,
-    locationData.sunset,
-    locationData.humidity,
-    locationData.precipitation,
-    locationData.icon,
-  );
+    const locationData = new Weather(
+      weatherData.resolvedAddress,
+      weatherData["currentConditions"].temp,
+      weatherData["currentConditions"].feelslike,
+      weatherData["currentConditions"].conditions,
+      weatherData["currentConditions"].humidity,
+      weatherData["currentConditions"].sunrise,
+      weatherData["currentConditions"].sunset,
+      weatherData.description,
+      weatherData["currentConditions"].icon,
+      weatherData["days"][0].precipprob,
+    );
+
+    console.log(locationData);
+
+    displayWeatherData(
+      locationData.address,
+      locationData.conditions,
+      locationData.temp,
+      locationData.feelsLike,
+      locationData.description,
+      locationData.sunrise,
+      locationData.sunset,
+      locationData.humidity,
+      locationData.precipitation,
+      locationData.icon,
+    );
+
+    errorDisplay.textContent = "";
+    weatherDisplay.style.display = "grid";
+    tempConversionButton.style.display = "block";
+  } catch {
+    errorDisplay.textContent = "Location Not Found, Try Again!";
+    weatherDisplay.style.display = "none";
+    tempConversionButton.style.display = "none";
+  }
 }
 
 export { getWeather };
